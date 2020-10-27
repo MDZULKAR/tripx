b143ffa9503a0494d6a6ea28e140eea1b4e9d6dd
MDZULKAR <48338573+MDZULKAR@users.noreply.github.com>
2020-10-27 20:54:15 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import * as firebase from "firebase";
+import { StyleSheet } from "react-native";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 import Cities from "./Components/Cities";
+import Cost from "./Components/Cost";
+import Plan from "./Components/Plan";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Planing from "./Components/Planing";
 const firebaseConfig = {
   apiKey: "AIzaSyCpkKt90aSY6BjqzBVa1Qrgcqx6PtVe0Rg",
   authDomain: "tripx-121c0.firebaseapp.com",
@@ -31,6 +35,9 @@ export default function App() {
         <Stack.Screen component={Login} name="Login" />
         <Stack.Screen component={Signup} name="Signup" />
         <Stack.Screen component={Cities} name="Cities" />
+        <Stack.Screen component={Cost} name="Cost" />
+        <Stack.Screen component={Plan} name="Plan" />
+        <Stack.Screen component={Planing} name="Planing" />
       </Stack.Navigator>
     </NavigationContainer>
   );
